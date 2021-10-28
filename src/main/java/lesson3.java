7a496a82c592442863f677db44219d964c24b235
danil2326 <92806387+danil2326@users.noreply.github.com>
2021-10-29 00:16:04 +0400

    Update lesson3.java

--- src/main/java/lesson3.java
+++ src/main/java/lesson3.java
@@ -4,45 +4,45 @@ import java.util.Arrays;
 
 public class lesson3 {
     public static void main(String[] args) {
+         mysteryNumbers();
     }
     
    
 
     //Задание1
-    public static void mysteryNumbers() {
-        Scanner sc = new Scanner(System.in);
-        int number = (int) (Math.random() * 10);
-        System.out.println("Компьютер загадал число от 0 до 9, угадайте число, у вас 3 попытки");
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Попытка " + (i + 1) + ": ");
-            int input = sc.nextInt();
-            while (true) {
-                if (input < number) {
+   public static void mysteryNumbers() {
+            Scanner sc = new Scanner(System.in);
+            int input = (int) (Math.random() * 10);
+            int number = (int) (Math.random() * 10);
+            System.out.println("Компьютер загадал число от 0 до 9, угадайте число, у вас 3 попытки");
+            for (int i = 0; i < 3; i++) {
+                System.out.println("Попытка " + (i + 1) + ": ");
+                input = sc.nextInt();
+                if (input < number)
                     System.out.println("Число больше");
-                    break;
-                }
-                if (input > number) {
+                if (input > number)
                     System.out.println("Число меньше");
-                    break;
-                }
                 if (input == number) {
                     System.out.println("Вы угадали");
-
+                    System.out.println("Повторить игру еще раз? 1 – да / 0 – нет");
+                    input = sc.nextInt();
+                    if (input == 1) {
+                        mysteryNumbers();
+                    } else {
+                        System.out.println("Спасибо за игру");
+                        break;
+                    }
                 }
-                System.out.println("Повторить игру еще раз? 1 – да / 0 – нет");
-                input = sc.nextInt();
-                if (input == 1) {
-                    mysteryNumbers();
-                } else System.out.println("Спасибо за игру");
-
-
             }
-
-
+            System.out.println("Вы не угадали :(");
+            System.out.println("Повторить игру еще раз? 1 – да / 0 – нет");
+            input = sc.nextInt();
+            if (input == 1) mysteryNumbers();
+            else {
+                System.out.println("Спасибо за игру!");
+            }
         }
     }
-}
-
 
 
 
