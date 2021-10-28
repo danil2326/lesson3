d56dab227aa8a36426b4eb401a26f1d84d0f4183
danil2326 <92806387+danil2326@users.noreply.github.com>
2021-10-28 23:29:48 +0400

    Update lesson3.java

--- src/main/java/lesson3.java
+++ src/main/java/lesson3.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class lesson3 {
     public static void main(String[] args) {
     }
-    // mysteryNumbers();
-
+    
+   
 
     //Задание1
     public static void mysteryNumbers() {
